821a73e9ae42dea8bb46cf2738774b601888d59a
Guvenc Gulce <guevenc.guelce@sap.com>
2022-03-30 09:08:45 +0200

    Introduce VF-to-VF automated scapy testing
    Introduce VF-to-VF automated scapy testing so that we
    can also test scenarios like VF-to-VF/PF routing and NAT.
    
    Signed-off-by: Guvenc Gulce <guevenc.guelce@sap.com>

--- test/test_ipv4.py
+++ test/test_ipv4.py
@@ -10,17 +10,6 @@ from config import *
 import multiprocessing
 import signal
 
-@pytest.fixture(scope="module")
-def add_machine(build_path):
-	add_machine_cmd = build_path+"/test/dp_grpc_client --addmachine 1 --vni 100 --ipv4 172.32.10.5 --ipv6 2001::10"
-	add_ipv4_route_cmd = build_path+"/test/dp_grpc_client --addroute 1 --vni 100 --ipv4 192.168.129.0 --length 24 --t_vni 100 --t_ipv6 2a10:afc0:e01f:f408::1"
-	subprocess.run(shlex.split("ip link set dev "+vf0_tap+" up"))
-	subprocess.run(shlex.split("ip link set dev "+pf0_tap+" up"))
-	subprocess.run(shlex.split(add_machine_cmd))
-	subprocess.run(shlex.split(add_ipv4_route_cmd))
-	time.sleep(1)
-
-
 def test_l2_arp(add_machine):
 	try:
 		arp_packet = Ether(dst=bcast_mac)/ARP(pdst=gw_ip4, hwdst=vf0_mac, psrc=null_ip)
@@ -33,7 +22,7 @@ def test_l2_arp(add_machine):
 		prepare_env.terminate()
 
 
-def test_dhcpv4(capsys,add_machine):
+def test_dhcpv4_vf0(capsys,add_machine):
 	conf.checkIPaddr = False
 	answer = dhcp_request(iface=vf0_tap, timeout=5)
 	resp = str(answer[DHCP].options[0][1])
@@ -42,23 +31,40 @@ def test_dhcpv4(capsys,add_machine):
 	dst_mac = answer[Ether].src
 	dst_ip = answer[IP].src
 	answer = srp1(Ether(dst=dst_mac) / IP(src=vf0_ip, dst=dst_ip) / UDP(sport=68, dport=67) /
-                BOOTP(chaddr=vf0_mac) / DHCP(options=[("message-type", "request"), "end"]), iface=vf0_tap)
+				BOOTP(chaddr=vf0_mac) / DHCP(options=[("message-type", "request"), "end"]), iface=vf0_tap)
 	print(str(answer[BOOTP].yiaddr))
 	assert (str(answer[BOOTP].yiaddr) == vf0_ip)
 
+def test_dhcpv4_vf1(capsys,add_machine):
+	conf.checkIPaddr = False
+	answer = dhcp_request(iface=vf1_tap, timeout=5)
+	resp = str(answer[DHCP].options[0][1])
+	print(str(resp))
+	pytest.assume(str(resp) == str(2))
+	dst_mac = answer[Ether].src
+	dst_ip = answer[IP].src
+	answer = srp1(Ether(dst=dst_mac) / IP(src=vf1_ip, dst=dst_ip) / UDP(sport=68, dport=67) /
+				BOOTP(chaddr=vf0_mac) / DHCP(options=[("message-type", "request"), "end"]), iface=vf1_tap)
+	print(str(answer[BOOTP].yiaddr))
+	assert (str(answer[BOOTP].yiaddr) == vf1_ip)
 
 def is_encaped_icmp_pkt(pkt):
-    if IPv6 in pkt:
-        pktipv6=pkt[IPv6]
-        if pktipv6.dst==ul_actual_dst and pktipv6.nh==4:
-            if ICMP in pkt:
-                return True
-    return False
+	if IPv6 in pkt:
+		pktipv6=pkt[IPv6]
+		if pktipv6.dst==ul_actual_dst and pktipv6.nh==4:
+			if ICMP in pkt:
+				return True
+	return False
 
 def is_icmp_pkt(pkt):
-    if ICMP in pkt:
-        return True
-    return False
+	if ICMP in pkt:
+		return True
+	return False
+
+def is_tcp_pkt(pkt):
+	if TCP in pkt:
+		return True
+	return False
 
 def ipv4_in_ipv6_responder():
 	pkt_list = sniff(count=1,lfilter=is_encaped_icmp_pkt,iface=pf0_tap)
@@ -90,3 +96,31 @@ def test_IPv4inIPv6(capsys,add_machine):
 	if len(pkt_list)==0:
 		raise AssertionError('Cannot receive icmp reply!')
 	
+
+def vf_to_vf_tcp_responder():
+	pkt_list = sniff(count=1,lfilter=is_tcp_pkt,iface=vf1_tap)
+	pkt=pkt_list[0]
+	pkt.show()
+	if Ether in pkt:
+		pktether=pkt[Ether]
+	if IP in pkt:
+		pktip= pkt[IP]
+
+	reply_pkt = Ether(dst=pktether.src,src=pktether.dst,type=0x0800)/IP(dst=pktip.src,src=pktip.dst)/TCP()
+	time.sleep(1)
+	sendp(reply_pkt, iface=vf1_tap)
+
+
+def test_vf_to_vf_tcp(capsys,add_machine):
+	d=multiprocessing.Process(name="sniffer",target=vf_to_vf_tcp_responder)
+	d.daemon=False
+	d.start()
+
+	time.sleep(1)
+
+	tcp_pkt = Ether(dst=vf1_mac,src=vf0_mac,type=0x0800)/IP(dst=vf1_ip,src=vf0_ip)/TCP()
+	sendp(tcp_pkt,iface=vf0_tap)
+	
+	pkt_list = sniff(count=1,lfilter=is_tcp_pkt,iface=vf0_tap,timeout=2)
+	if len(pkt_list)==0:
+		raise AssertionError('Cannot receive icmp reply!')
--- test/test_ipv6.py
+++ test/test_ipv6.py
@@ -8,71 +8,60 @@ import pytest, shlex, subprocess, time
 from config import *
 import multiprocessing
 
-@pytest.fixture()
-def add_machine(build_path):
-	add_machine_cmd = build_path+"/test/dp_grpc_client --addmachine 1 --vni 100 --ipv4 172.32.10.5 --ipv6 2001::10"
-	add_ipv6_route_cmd = build_path+"/test/dp_grpc_client --addroute 1 --vni 100 --ipv6 2002::123 --length 128 --t_vni 100 --t_ipv6 2a10:afc0:e01f:f408::1"
-	subprocess.run(shlex.split("ip link set dev "+vf0_tap+" up"))
-	subprocess.run(shlex.split("ip link set dev "+pf0_tap+" up"))
-	subprocess.run(shlex.split(add_machine_cmd))
-	time.sleep(1)
-	subprocess.run(shlex.split(add_ipv6_route_cmd))
-	time.sleep(1)
-
 
 def test_nd(add_machine):
-    answer = neighsol(gw_ip6, vf0_ipv6, iface=vf0_tap, timeout=2)
-    mac = str(answer[ICMPv6NDOptDstLLAddr].lladdr)
-    print(mac)
-    assert(mac == vf0_mac)
+	answer = neighsol(gw_ip6, vf0_ipv6, iface=vf0_tap, timeout=2)
+	mac = str(answer[ICMPv6NDOptDstLLAddr].lladdr)
+	print(mac)
+	assert(mac == vf0_mac)
 
 def test_dhcp6(capsys):
-    eth = Ether(dst=mc_mac)
-    ip6 = IPv6(dst=gw_ip6)
-    udp = UDP(sport=546,dport=547)
-    sol = DHCP6_Solicit()
-    req = DHCP6_Request()
-    
-    sol.trid = random.randint(0,16777215)
-    rc_op = DHCP6OptRapidCommit(optlen=0)
-    opreq = DHCP6OptOptReq()
-    et_op= DHCP6OptElapsedTime()
-    cid_op = DHCP6OptClientId()
-    iana_op = DHCP6OptIA_NA(iaid=0x18702501)
+	eth = Ether(dst=mc_mac)
+	ip6 = IPv6(dst=gw_ip6)
+	udp = UDP(sport=546,dport=547)
+	sol = DHCP6_Solicit()
+	req = DHCP6_Request()
 
-    iana_op.optlen = 12
-    iana_op.T1 = 0
-    iana_op.T2 = 0
-    cid_op.optlen = 28
-    cid_op.duid = "00020000ab11b7d4e0eed266171d"
-    opreq.optlen = 4
+	sol.trid = random.randint(0,16777215)
+	rc_op = DHCP6OptRapidCommit(optlen=0)
+	opreq = DHCP6OptOptReq()
+	et_op= DHCP6OptElapsedTime()
+	cid_op = DHCP6OptClientId()
+	iana_op = DHCP6OptIA_NA(iaid=0x18702501)
 
-    pkt = eth/ip6/udp/sol/iana_op/rc_op/et_op/cid_op/opreq
-    answer = srp1(pkt, iface=vf0_tap, type=ETH_P_IPV6, timeout=2)
-    print(str(answer[DHCP6OptIAAddress].addr))
-    pytest.assume(str(cid_op.duid) == str(answer[DHCP6OptClientId].duid))
+	iana_op.optlen = 12
+	iana_op.T1 = 0
+	iana_op.T2 = 0
+	cid_op.optlen = 28
+	cid_op.duid = "00020000ab11b7d4e0eed266171d"
+	opreq.optlen = 4
 
-    iana_op = answer[DHCP6OptIAAddress]
-    pkt = eth/ip6/udp/req/iana_op/rc_op/et_op/cid_op/opreq
-    answer = srp1(pkt, iface=vf0_tap, type=ETH_P_IPV6, timeout=2)
-    pytest.assume(str(cid_op.duid) == str(answer[DHCP6OptClientId].duid))
-    print(str(answer[DHCP6OptIAAddress].addr))
-    assert(str(answer[DHCP6OptIAAddress].addr) == vf0_ipv6)
+	pkt = eth/ip6/udp/sol/iana_op/rc_op/et_op/cid_op/opreq
+	answer = srp1(pkt, iface=vf0_tap, type=ETH_P_IPV6, timeout=2)
+	print(str(answer[DHCP6OptIAAddress].addr))
+	pytest.assume(str(cid_op.duid) == str(answer[DHCP6OptClientId].duid))
+
+	iana_op = answer[DHCP6OptIAAddress]
+	pkt = eth/ip6/udp/req/iana_op/rc_op/et_op/cid_op/opreq
+	answer = srp1(pkt, iface=vf0_tap, type=ETH_P_IPV6, timeout=2)
+	pytest.assume(str(cid_op.duid) == str(answer[DHCP6OptClientId].duid))
+	print(str(answer[DHCP6OptIAAddress].addr))
+	assert(str(answer[DHCP6OptIAAddress].addr) == vf0_ipv6)
 
 
 
 def is_encaped_icmpv6_pkt(pkt):
-    if IPv6 in pkt:
-        pktipv6=pkt[IPv6]
-        if pktipv6.dst==ul_actual_dst and pktipv6.nh==0x29:
-            # if ICMP in pkt:
-            return True
-    return False
+	if IPv6 in pkt:
+		pktipv6=pkt[IPv6]
+		if pktipv6.dst==ul_actual_dst and pktipv6.nh==0x29:
+			# if ICMP in pkt:
+			return True
+	return False
 
 def is_icmpv6echo_pkt(pkt):
-    if ICMPv6EchoReply in pkt:
-        return True
-    return False
+	if ICMPv6EchoReply in pkt:
+		return True
+	return False
 
 def ipv6_in_ipv6_responder():
 	pkt_list = sniff(count=1,lfilter=is_encaped_icmpv6_pkt,iface=pf0_tap)
