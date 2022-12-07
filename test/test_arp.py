b3326f3b7fae3c07468f9f124e47201a3afbfc61
Jaromír Smrček <4plague@gmail.com>
2022-12-07 16:55:42 +0100

    Fix minor bugs in test suite

--- test/test_arp.py
+++ test/test_arp.py
@@ -5,6 +5,8 @@ def test_l2_arp(prepare_ifaces):
 	arp_packet = (Ether(dst=bcast_mac) /
 				  ARP(pdst=gw_ip4, hwdst=vf0_mac, psrc=null_ip))
 	answer, unanswered = srp(arp_packet, iface=vf0_tap, type=ETH_P_ARP, timeout=2)
+	assert len(answer) == 1, \
+		"No ARP response"
 	for sent, received in answer:
 		src_mac = received[ARP].hwsrc
 		assert src_mac == vf0_mac, \
