792714145f1f03b63db3e6629ce082c0a7874874
Jaromír Smrček <4plague@gmail.com>
2023-02-02 17:48:46 +0100

    Simplified NAT pytest code

--- test/test_vf_to_pf.py
+++ test/test_vf_to_pf.py
@@ -45,7 +45,7 @@ def test_vf_to_pf_network_nat_icmp(prepare_ipv4, grpc_client):
 		"NAT deleted")
 
 
-def reply_tcp_pkt_from_vm1():
+def reply_tcp_if_port_not(forbidden_port):
 
 	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=pf0_tap, timeout=2)
 	assert len(pkt_list) == 1, \
@@ -57,88 +57,48 @@ def reply_tcp_pkt_from_vm1():
 	assert src_ip == nat_vip and sport >= nat_local_min_port and sport < nat_local_max_port, \
 		f"Bad TCP packet (ip: {src_ip}, sport: {sport})"
 
-	reply_pkt = (Ether(dst=pkt[Ether].src, src=pkt[Ether].dst, type=0x86DD) /
-				 IPv6(dst=ul_actual_src, src=pkt[IPv6].dst, nh=4) /
-				 IP(dst=pkt[IP].src, src=pkt[IP].dst) /
-				 TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport))
-	delayed_sendp(reply_pkt, pf0_tap)
+	if forbidden_port is None or sport != forbidden_port:
+		reply_pkt = (Ether(dst=pkt[Ether].src, src=pkt[Ether].dst, type=0x86DD) /
+					 IPv6(dst=ul_actual_src, src=pkt[IPv6].dst, nh=4) /
+					 IP(dst=pkt[IP].src, src=pkt[IP].dst) /
+					 TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport))
+		delayed_sendp(reply_pkt, pf0_tap)
+
+	return sport
+
+def reply_tcp_pkt_from_vm1():
+	reply_tcp_if_port_not(None)
 
 def reply_tcp_pkt_from_vm1_max_port():
+	sport = reply_tcp_if_port_not(None)
+	reply_tcp_if_port_not(sport)
 
-	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=pf0_tap, timeout=2)
+def send_tcp_through_port(port):
+
+	tcp_pkt = (Ether(dst=pf0_mac, src=vf0_mac, type=0x0800) /
+			   IP(dst=public_ip, src=vf0_ip) /
+			   TCP(sport=port))
+	delayed_sendp(tcp_pkt, vf0_tap)
+
+	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=vf0_tap, timeout=2)
 	assert len(pkt_list) == 1, \
 		"No network-natted TCP packet received on PF"
 
 	pkt = pkt_list[0]
-	src_ip = pkt[IP].src
-	sport = pkt[TCP].sport
-	first_port = sport
-	assert src_ip == nat_vip and sport >= nat_local_min_port and sport < nat_local_max_port, \
-		f"Bad TCP packet (ip: {src_ip}, sport: {sport})"
+	dst_ip = pkt[IP].dst
+	dport = pkt[TCP].dport
+	assert dst_ip == vf0_ip and dport == port, \
+		f"Bad TCP packet (ip: {dst_ip}, dport: {dport})"
 
-	reply_pkt = (Ether(dst=pkt[Ether].src, src=pkt[Ether].dst, type=0x86DD) /
-				 IPv6(dst=ul_actual_src, src=pkt[IPv6].dst, nh=4) /
-				 IP(dst=pkt[IP].src, src=pkt[IP].dst) /
-				 TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport))
-	delayed_sendp(reply_pkt, pf0_tap)
-
-	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=pf0_tap, timeout=2)
-	assert len(pkt_list) == 1, \
-		"No network-natted TCP packet received on PF"
-
-	pkt = pkt_list[0]
-	src_ip = pkt[IP].src
-	sport = pkt[TCP].sport
-	second_port = sport
-	assert src_ip == nat_vip and sport >= nat_local_min_port and sport < nat_local_max_port, \
-		f"Bad TCP packet (ip: {src_ip}, sport: {sport})"
-	
-	if second_port != first_port:
-		reply_pkt = (Ether(dst=pkt[Ether].src, src=pkt[Ether].dst, type=0x86DD) /
-					IPv6(dst=ul_actual_src, src=pkt[IPv6].dst, nh=4) /
-					IP(dst=pkt[IP].src, src=pkt[IP].dst) /
-					TCP(sport=pkt[TCP].dport, dport=pkt[TCP].sport))
-		delayed_sendp(reply_pkt, pf0_tap)
 
 def test_vf_to_pf_network_nat_max_port_tcp(prepare_ipv4, grpc_client):
 
-	# TODO(plague) I suspect that there is an occasional problem here
-	# as this test is called immediately after the previous one, that established and then teared down the same route
-	# there will be a race condition and the service needs more time this time around
 	grpc_client.assert_output(f"--addnat {vm1_name} --ipv4 {nat_vip} --min_port {nat_local_min_port} --max_port {nat_local_max_port}",
 		"Received underlay route")
 
 	threading.Thread(target=reply_tcp_pkt_from_vm1_max_port).start()
-
-	tcp_pkt = (Ether(dst=pf0_mac, src=vf0_mac, type=0x0800) /
-			   IP(dst=public_ip, src=vf0_ip) /
-			   TCP(sport=1240))
-	delayed_sendp(tcp_pkt, vf0_tap)
-
-	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=vf0_tap, timeout=2)
-	assert len(pkt_list) == 1, \
-		"No network-natted TCP packet received on PF"
-
-	pkt = pkt_list[0]
-	dst_ip = pkt[IP].dst
-	dport = pkt[TCP].dport
-	assert dst_ip == vf0_ip and dport == 1240, \
-		f"Bad TCP packet (ip: {dst_ip}, dport: {dport})"
-
-	tcp_pkt = (Ether(dst=pf0_mac, src=vf0_mac, type=0x0800) /
-			   IP(dst=public_ip, src=vf0_ip) /
-			   TCP(sport=1241))
-	delayed_sendp(tcp_pkt, vf0_tap)
-
-	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=vf0_tap, timeout=2)
-	assert len(pkt_list) == 1, \
-		"No network-natted TCP packet received on PF"
-
-	pkt = pkt_list[0]
-	dst_ip = pkt[IP].dst
-	dport = pkt[TCP].dport
-	assert dst_ip == vf0_ip and dport == 1241, \
-		f"Bad TCP packet (ip: {dst_ip}, dport: {dport})"
+	send_tcp_through_port(1240)
+	send_tcp_through_port(1241)
 
 	grpc_client.assert_output(f"--delnat {vm1_name}",
 		"NAT deleted")
@@ -146,33 +106,16 @@ def test_vf_to_pf_network_nat_max_port_tcp(prepare_ipv4, grpc_client):
 
 def test_vf_to_pf_network_nat_tcp(prepare_ipv4, grpc_client):
 
-	# TODO(plague) I suspect that there is an occasional problem here
-	# as this test is called immediately after the previous one, that established and then teared down the same route
-	# there will be a race condition and the service needs more time this time around
 	grpc_client.assert_output(f"--addnat {vm1_name} --ipv4 {nat_vip} --min_port {nat_local_min_port} --max_port {nat_local_max_port}",
 		"Received underlay route")
 
 	threading.Thread(target=reply_tcp_pkt_from_vm1).start()
-
-	tcp_pkt = (Ether(dst=pf0_mac, src=vf0_mac, type=0x0800) /
-			   IP(dst=public_ip, src=vf0_ip) /
-			   TCP(sport=1240))
-	delayed_sendp(tcp_pkt, vf0_tap)
-
-	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=vf0_tap, timeout=2)
-	assert len(pkt_list) == 1, \
-		"No network-natted TCP packet received on PF"
-
-	pkt = pkt_list[0]
-	dst_ip = pkt[IP].dst
-	dport = pkt[TCP].dport
-	assert dst_ip == vf0_ip and dport == 1240, \
-		f"Bad TCP packet (ip: {dst_ip}, dport: {dport})"
+	send_tcp_through_port(1240)
 
 	grpc_client.assert_output(f"--delnat {vm1_name}",
 		"NAT deleted")
 
-# TODO: this can be done better with sniff(iface=[iface1, iface2], but ther is a regression in scany 2.4.5 that broke this
+# TODO: this can be done better with sniff(iface=[iface1, iface2], but there is a regression in scapy 2.4.5 that broke this
 def encaped_tcp_in_ipv6_vip_responder(pf_name):
 	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=pf_name, timeout=2)
 	# with --port-redundancy, there are two listeners running and only one receives a packet
@@ -208,7 +151,7 @@ def test_vf_to_pf_vip_snat(prepare_ipv4, grpc_client, port_redundancy):
 		"VIP deleted")
 
 
-# TODO: this can be done better with sniff(iface=[iface1, iface2], but ther is a regression in scany 2.4.5 that broke this
+# TODO: this can be done better with sniff(iface=[iface1, iface2], but there is a regression in scapy 2.4.5 that broke this
 def reply_with_icmp_err_fragment_needed(pf_name):
 	pkt_list = sniff(count=1, lfilter=is_tcp_pkt, iface=pf_name, timeout=2)
 	# with --port-redundancy, there are two listeners running and only one receives a packet
