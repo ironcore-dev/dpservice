9dec9285a21bd321c2118501efd6b9ff34e1e2ea
Tao <tao.li06@sap.com>
2023-05-15 17:15:33 +0200

    offload to send back net nat's neighboring traffic

--- include/rte_flow/dp_rte_flow_traffic_forward.h
+++ include/rte_flow/dp_rte_flow_traffic_forward.h
@@ -17,7 +17,7 @@ extern "C"
 
 #define DP_TUNN_IPIP_ENCAP_SIZE sizeof(struct rte_ether_hdr)+sizeof(struct rte_ipv6_hdr)
 
-int dp_handle_traffic_forward_offloading(struct rte_mbuf *m,struct dp_flow *df);
+int dp_offload_handler(struct rte_mbuf *m,struct dp_flow *df);
 
 #ifdef __cplusplus
 }
