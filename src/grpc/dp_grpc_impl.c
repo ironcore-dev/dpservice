d3b67932a6ffe8206ea1e19f2ca1035960509f0a
Tao <tao.li06@sap.com>
2023-05-25 12:27:46 +0200

    enable underlay IPv6 addr without randomness to assist development.

--- src/grpc/dp_grpc_impl.c
+++ src/grpc/dp_grpc_impl.c
@@ -52,8 +52,18 @@ static __rte_always_inline void dp_generate_underlay_ipv6(uint8_t *route)
 	rte_memcpy(route, get_underlay_conf()->src_ip6, DP_VNF_IPV6_ADDR_SIZE);
 	/* Following 2 bytes for kernel routing and 1 byte reserved */
 	memset(route + 8, 0, 3);
-	/* 1 byte random value */
-	rte_memcpy(route + 11, &random_byte, 1);
+
+	#ifdef ENABLE_STATIC_UNDERLAY_IP
+		/* 1 byte static value */
+		uint8_t static_byte = 0x01;
+
+		rte_memcpy(route + 11, &static_byte, 1);
+		RTE_SET_USED(random_byte);
+	#else
+		/* 1 byte random value */
+		rte_memcpy(route + 11, &random_byte, 1);
+	#endif
+
 	/* 4 byte counter */
 	rte_memcpy(route + 12, &local, 4);
 
