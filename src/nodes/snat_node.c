ironcore-dev/dpservice

commit 59a0b697a11532e6bcbd068f61d6a6ebdd38a3f5

Fix clang conditional-uninitialized

src/nodes/snat_node.c
@@ -49,13 +49,13 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 									   DP_LOG_IPV4(src_ip), DP_LOG_PORT(ntohs(df->l4_info.trans_port.src_port)));
 					return SNAT_NEXT_DROP;
 				}
-				nat_port = htons((uint16_t)ret);
+				nat_port = (uint16_t)ret;
 				ipv4_hdr->src_addr = htonl(snat_data->network_nat_ip);
 
 				DP_STATS_NAT_INC_USED_PORT_CNT(m->port);
 
 				if (df->l4_type == DP_IP_PROTO_ICMP) {
-					if (dp_change_icmp_identifier(m, ntohs(nat_port)) == DP_IP_ICMP_ID_INVALID) {
+					if (dp_change_icmp_identifier(m, nat_port) == DP_IP_ICMP_ID_INVALID) {
 						DPNODE_LOG_WARNING(node, "Cannot replace ICMP header's identifier");
 						return SNAT_NEXT_DROP;
 					}
@@ -63,7 +63,7 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 					cntrack->offload_flags.reply = DP_FLOW_OFFLOADED;
 					df->flags.offload_decision = DP_FLOW_NON_OFFLOAD;
 				} else {
-					if (dp_change_l4_hdr_port(m, DP_L4_PORT_DIR_SRC, nat_port) == 0) {
+					if (dp_change_l4_hdr_port(m, DP_L4_PORT_DIR_SRC, htons(nat_port)) == 0) {
 						DPNODE_LOG_WARNING(node, "Cannot replace L4 header's src port");
 						return SNAT_NEXT_DROP;
 					}
@@ -73,19 +73,18 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 				cntrack->nf_info.vni = vni;
 				cntrack->nf_info.l4_type = df->l4_type;
 				cntrack->nf_info.icmp_err_ip_cksum = ipv4_hdr->hdr_checksum;
+				df->nat_port = nat_port;
+				/* Expect the new destination in this conntrack object */
+				cntrack->flow_key[DP_FLOW_DIR_REPLY].port_dst = nat_port;
 			}
 			df->flags.nat = DP_NAT_CHG_SRC_IP;
 			df->nat_addr = ipv4_hdr->src_addr; // nat_addr is the new src_addr in ipv4_hdr
-			if (snat_data->network_nat_ip != 0)
-				df->nat_port = ntohs(nat_port);
 			dp_nat_chg_ip(df, ipv4_hdr, m);
 
 			/* Expect the new destination in this conntrack object */
 			cntrack->flow_status |= DP_FLOW_STATUS_FLAG_SRC_NAT;;
 			dp_delete_flow_key(&cntrack->flow_key[DP_FLOW_DIR_REPLY]);
 			cntrack->flow_key[DP_FLOW_DIR_REPLY].ip_dst = ntohl(ipv4_hdr->src_addr);
-			if (snat_data->network_nat_ip != 0)
-				cntrack->flow_key[DP_FLOW_DIR_REPLY].port_dst = ntohs(nat_port);
 
 			dp_add_flow(&cntrack->flow_key[DP_FLOW_DIR_REPLY]);
 			dp_add_flow_data(&cntrack->flow_key[DP_FLOW_DIR_REPLY], cntrack);

include/rte_flow/dp_rte_flow.h
@@ -55,7 +55,7 @@ struct rte_icmp_hdr *dp_get_icmp_hdr(struct rte_mbuf *m, uint16_t offset);
 void dp_get_icmp_err_ip_hdr(struct rte_mbuf *m, struct dp_icmp_err_ip_info *err_ip_info);
 void dp_change_icmp_err_l4_src_port(struct rte_mbuf *m, struct dp_icmp_err_ip_info *err_ip_info, uint16_t src_port_v);
 
-uint16_t dp_change_l4_hdr_port(struct rte_mbuf *m, uint8_t port_type, uint16_t new_val);
+uint16_t dp_change_l4_hdr_port(struct rte_mbuf *m, uint8_t port_type, rte_be16_t new_val);
 
 #define DP_IP_ICMP_ID_INVALID 0xFFFF
 uint16_t dp_change_icmp_identifier(struct rte_mbuf *m, uint16_t new_identifier);

src/nodes/conntrack_node.c
@@ -240,7 +240,7 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 	struct rte_tcp_hdr *tcp_hdr;
 	struct dp_flow *df;
 	struct flow_key *key;
-	bool key_cmp_result;
+	bool same_key;
 	int ret;
 
 	df = dp_get_flow_ptr(m);
@@ -268,11 +268,10 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 		if (unlikely(DP_FAILED(dp_build_flow_key(key, m))))
 			return CONNTRACK_NEXT_DROP;
 
-		if (prev_key)
-			key_cmp_result = dp_test_next_n_bytes_identical((const unsigned char *)prev_key,
-													(const unsigned char *)curr_key,
-													sizeof(struct flow_key));
-		if (!prev_key || !key_cmp_result) {
+		same_key = prev_key && dp_test_next_n_bytes_identical((const unsigned char *)prev_key,
+															  (const unsigned char *)curr_key,
+															  sizeof(struct flow_key));
+		if (!same_key) {
 			ret = dp_get_flow_data(key, (void **)&flow_val);
 			if (unlikely(DP_FAILED(ret))) {
 				if (likely(ret == -ENOENT)) {

src/rte_flow/dp_rte_flow.c
@@ -258,7 +258,7 @@ void dp_change_icmp_err_l4_src_port(struct rte_mbuf *m, struct dp_icmp_err_ip_in
 	}
 }
 
-uint16_t dp_change_l4_hdr_port(struct rte_mbuf *m, uint8_t port_type, uint16_t new_val)
+uint16_t dp_change_l4_hdr_port(struct rte_mbuf *m, uint8_t port_type, rte_be16_t new_val)
 {
 
 	struct dp_flow *df;

commit 01a113230e031853a0d562f3c31d588b848b2f39

fix snat bug leading to test error
caused by commit 59a0b69

src/nodes/snat_node.c
@@ -74,8 +74,7 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 				cntrack->nf_info.l4_type = df->l4_type;
 				cntrack->nf_info.icmp_err_ip_cksum = ipv4_hdr->hdr_checksum;
 				df->nat_port = nat_port;
-				/* Expect the new destination in this conntrack object */
-				cntrack->flow_key[DP_FLOW_DIR_REPLY].port_dst = nat_port;
+				
 			}
 			df->flags.nat = DP_NAT_CHG_SRC_IP;
 			df->nat_addr = ipv4_hdr->src_addr; // nat_addr is the new src_addr in ipv4_hdr
@@ -85,6 +84,10 @@ static __rte_always_inline rte_edge_t get_next_index(struct rte_node *node, stru
 			cntrack->flow_status |= DP_FLOW_STATUS_FLAG_SRC_NAT;;
 			dp_delete_flow_key(&cntrack->flow_key[DP_FLOW_DIR_REPLY]);
 			cntrack->flow_key[DP_FLOW_DIR_REPLY].ip_dst = ntohl(ipv4_hdr->src_addr);
+			
+			/* Expect the new destination in this conntrack object */
+			if (snat_data->network_nat_ip != 0)
+				cntrack->flow_key[DP_FLOW_DIR_REPLY].port_dst = nat_port;
 
 			dp_add_flow(&cntrack->flow_key[DP_FLOW_DIR_REPLY]);
 			dp_add_flow_data(&cntrack->flow_key[DP_FLOW_DIR_REPLY], cntrack);