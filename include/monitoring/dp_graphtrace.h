e85c4b9bfa4fb4d1fb2f653dc09ef93bfc4eacf3
Tao <tao.li06@sap.com>
2023-08-11 15:09:41 +0200

    enhancement code for auto start/stop graphtrace

--- include/monitoring/dp_graphtrace.h
+++ include/monitoring/dp_graphtrace.h
@@ -49,26 +49,26 @@ void _dp_graphtrace_send(struct rte_node *node, struct rte_node *next_node, void
 #define dp_graphtrace_node(NODE, OBJ) _dp_graphtrace_log_node(NODE, OBJ)
 #define dp_graphtrace_node_burst(NODE, OBJS, NUM) _dp_graphtrace_log_node_burst(NODE, OBJS, NUM)
 
-extern bool _dp_graphtrace_send_flag;
+extern bool dp_graphtrace_enabled;
 int dp_graphtrace_send_client_request_sync(uint8_t action, uint8_t dump_type, struct dp_graphtrace_mp_reply *reply);
 
 static __rte_always_inline void dp_graphtrace_next(struct rte_node *node, void *obj, rte_edge_t next_index)
 {
-	if (_dp_graphtrace_send_flag)
+	if (dp_graphtrace_enabled)
 		_dp_graphtrace_send(node, node->nodes[next_index], &obj, 1);
 	_dp_graphtrace_log_next(node, obj, next_index);
 }
 
 static __rte_always_inline void dp_graphtrace_next_burst(struct rte_node *node, void **objs, uint16_t nb_objs, rte_edge_t next_index)
 {
-	if (_dp_graphtrace_send_flag)
+	if (dp_graphtrace_enabled)
 		_dp_graphtrace_send(node, node->nodes[next_index], objs, nb_objs);
 	_dp_graphtrace_log_next_burst(node, objs, nb_objs, next_index);
 }
 
 static __rte_always_inline void dp_graphtrace_tx_burst(struct rte_node *node, void **objs, uint16_t nb_objs, uint16_t port_id)
 {
-	if (_dp_graphtrace_send_flag)
+	if (dp_graphtrace_enabled)
 		_dp_graphtrace_send(node, NULL, objs, nb_objs);
 	RTE_SET_USED(port_id);
 	_dp_graphtrace_log_tx_burst(node, objs, nb_objs, port_id);
@@ -76,7 +76,7 @@ static __rte_always_inline void dp_graphtrace_tx_burst(struct rte_node *node, vo
 
 static __rte_always_inline void dp_change_graphtrace_enable_flag(int enable)
 {
-	_dp_graphtrace_send_flag = enable;
+	dp_graphtrace_enabled = enable;
 }
 
 #ifdef __cplusplus
