8b38e4a8edb5c3ff32330705caa6b8577a1113ed
Tao <tao.li06@sap.com>
2023-09-06 16:47:01 +0200

    configure flow rules via monitoring queue to avoid race condition

--- src/monitoring/dp_monitoring.c
+++ src/monitoring/dp_monitoring.c
@@ -8,11 +8,17 @@ void dp_process_event_msg(struct rte_mbuf *m)
 	struct dp_event_msg *event_msg = rte_pktmbuf_mtod(m, struct dp_event_msg *);
 
 	switch (event_msg->msg_head.type) {
-	case DP_STATUS_TYPE_LINK:
+	case DP_EVENT_TYPE_LINK_STATUS:
 		dp_process_event_link_msg(m);
 		break;
-	case DP_STATUS_TYPE_FLOW_AGING:
+	case DP_EVENT_TYPE_FLOW_AGING:
 		dp_process_event_flow_aging_msg(m);
+		break;
+	case DP_EVENT_TYPE_HARDWARE_CAPTURE_START:
+		dp_process_event_hardware_capture_start_msg(m);
+		break;
+	case DP_EVENT_TYPE_HARDWARE_CAPTURE_STOP:
+		dp_process_event_hardware_capture_stop_msg(m);
 		break;
 	default:
 		DPS_LOG_WARNING("Unknown monitoring status message type", DP_LOG_VALUE(event_msg->msg_head.type));
