c049ada285e954786938a05d938c56901612770a
Jaromír Smrček <4plague@gmail.com>
2023-11-17 14:30:00 +0100

    Simpler VNF-exists test func

--- include/dp_vnf.h
+++ include/dp_vnf.h
@@ -12,8 +12,7 @@ extern "C" {
 #endif
 
 #define DP_VNF_MAX_TABLE_SIZE 1000
-#define DP_VNF_MATCH_ALL_PORT_ID_VALUE 0xFFFF
-#define DP_VNF_MATCH_ALL_PORT_ID true
+#define DP_VNF_MATCH_ALL_PORT_IDS 0xFFFF
 
 enum vnf_type {
 	DP_VNF_TYPE_UNDEFINED,
@@ -25,7 +24,7 @@ enum vnf_type {
 	DP_VNF_TYPE_INTERFACE_IP,
 };
 
-struct dp_vnf_alias {
+struct dp_vnf_prefix {
 	uint32_t	ip;
 	uint16_t	length;
 };
@@ -34,7 +33,7 @@ struct dp_vnf_value {
 	enum vnf_type		v_type;
 	uint32_t			vni;
 	uint16_t			portid;
-	struct dp_vnf_alias	alias_pfx;
+	struct dp_vnf_prefix	alias_pfx;
 };
 
 int dp_vnf_init(int socket_id);
@@ -45,8 +44,9 @@ const struct dp_vnf_value *dp_get_vnf_value(const uint8_t ul_addr6[DP_VNF_IPV6_A
 int dp_del_vnf_with_addr(const uint8_t ul_addr6[DP_VNF_IPV6_ADDR_SIZE]);
 int dp_del_vnf_with_value(const struct dp_vnf_value *val);
 int dp_find_vnf_with_value(const struct dp_vnf_value *val);
-int dp_get_vnf_entry(struct dp_vnf_value *val, enum vnf_type v_type, const struct dp_port *port, bool match_all);
 int dp_list_vnf_alias_routes(uint16_t port_id, enum vnf_type v_type, struct dp_grpc_responder *responder);
+
+bool dp_vnf_lbprefix_exists(struct dp_vnf_prefix *prefix, uint32_t vni, uint16_t port_id);
 
 #ifdef __cplusplus
 }
