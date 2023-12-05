4b15398d69eac656765a074a13565f09e9305a8d
Guvenc Gulce <guevenc.guelce@sap.com>
2023-12-05 10:53:19 +0100

    Shebang needs to be the first line in the file. Fix in dp_service.py
    Signed-off-by: Guvenc Gulce <guevenc.guelce@sap.com>

--- test/dp_service.py
+++ test/dp_service.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python3
+
 # SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
 # SPDX-License-Identifier: Apache-2.0
-
-#!/usr/bin/env python3
 
 import os
 import shlex
