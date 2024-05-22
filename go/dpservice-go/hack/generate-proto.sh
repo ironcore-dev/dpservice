a4347d23a8d7eea6dd2a1686a057b51b3efc6650
Viliam Lorinc <viliam.lorinc@t-systems.com>
2024-05-22 16:52:51 +0200

    add gitignore to dpservice-go, change generate to use local dpservice

--- go/dpservice-go/hack/generate-proto.sh
+++ go/dpservice-go/hack/generate-proto.sh
@@ -2,8 +2,6 @@
 # SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
 # SPDX-License-Identifier: Apache-2.0
 
-NET_DPSERVICE_REVISION="main"
-
 set -e
 
 PROTOC_GEN_GO="$PROTOC_GEN_GO"
@@ -11,36 +9,20 @@ PROTOC_GEN_GO_GRPC="$PROTOC_GEN_GO_PROTOC_GEN_GO_GRPC"
 
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-NET_DPSERVICE_DIR="$(mktemp -d)"
-trap 'rm -rf $NET_DPSERVICE_DIR' EXIT
-
-function clone() {
-  cd "$NET_DPSERVICE_DIR"
-  git init
-  git remote add origin git@github.com:ironcore-dev/dpservice.git
-  git fetch origin "$NET_DPSERVICE_REVISION" --depth=1
-  git reset --hard FETCH_HEAD
-  git fetch --prune --unshallow
-  git describe --tags > version.txt
-}
-
-echo "Cloning repository"
-if ! err="$(clone 2>&1)"; then
-  echo "Error cloning repository:"
-  echo "$err"
-fi
-
-cp $NET_DPSERVICE_DIR/version.txt ./proto/generated_from.txt
+DPSERVICE_DIR="../.."
+cd "$DPSERVICE_DIR"
+git describe --tags > ./go/dpservice-go/proto/generated_from.txt
+cd ./go/dpservice-go/
 
 echo "Generating protobuf"
 export PATH="$PATH:$(dirname "$PROTOC_GEN_GO")"
 export PATH="$PATH:$(dirname "$PROTOC_GEN_GO_GRPC")"
-protoc --proto_path="$NET_DPSERVICE_DIR" \
+protoc --proto_path="$DPSERVICE_DIR" \
   --go_out="$BASEDIR"/.. \
   --go_opt=paths=source_relative \
   --go-grpc_out="$BASEDIR"/.. \
   --go-grpc_opt=paths=source_relative \
-  "$NET_DPSERVICE_DIR"/proto/dpdk.proto
+  "$DPSERVICE_DIR"/proto/dpdk.proto
 
 for file in "$BASEDIR"/../proto/*.pb.go; do
   boilerplate="$(cat "$BASEDIR"/boilerplate.go.txt)"
