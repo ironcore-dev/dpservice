8900dd7f804137becc785eb8498e6baf8b350cdb
Jaromír Smrček <4plague@gmail.com>
2024-06-05 14:26:59 +0200

    Fix empty version string for main branch without tags

--- hack/get_version.sh
+++ hack/get_version.sh
@@ -3,7 +3,7 @@
 # For non-tagged commits, this is a simple "vX.X.X-XX-gXXXXXXX"
 # For dpservice-bin tagged commits, this is "vX.X.X"
 # For dpservice-go tagged commits, this is "go/dpservice-go/vX.X.X-XX-gXXXXXXX"
-GITVER=$(git describe --tags)
+GITVER=$(git describe --tags --always)
 
 # The solution is to simply print the last item of an array created by splitting the Git version
 ARRAY=(${GITVER//\// })
