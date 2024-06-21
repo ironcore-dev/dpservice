f1e942d564e874711161ed168e510d87786d484b
Jaromír Smrček <4plague@gmail.com>
2024-06-21 17:46:28 +0200

    Test commit with refcount validation

--- include/dp_refcount.h
+++ include/dp_refcount.h
@@ -14,28 +14,33 @@ extern "C" {
 struct dp_ref {
 	rte_atomic32_t refcount;
 	void (*release)(struct dp_ref *dpref);
+	bool valid;
 };
 
 static inline void dp_ref_init(struct dp_ref *ref, void (*release)(struct dp_ref *dpref))
 {
 	rte_atomic32_set(&ref->refcount, 1);
 	ref->release = release;
+	ref->valid = true;
 }
 
 static inline void dp_ref_inc(struct dp_ref *ref)
 {
+	RTE_VERIFY(ref->valid);
 	RTE_VERIFY(rte_atomic32_read(&ref->refcount));
 	rte_atomic32_add(&ref->refcount, 1);
 }
 
 static inline void dp_ref_dec(struct dp_ref *ref)
 {
+	RTE_VERIFY(ref->valid);
 	if (rte_atomic32_dec_and_test(&ref->refcount))
 		ref->release(ref);
 }
 
 static inline bool dp_ref_dec_and_chk_freed(struct dp_ref *ref)
 {
+	RTE_VERIFY(ref->valid);
 	if (rte_atomic32_dec_and_test(&ref->refcount)) {
 		ref->release(ref);
 		return true;
