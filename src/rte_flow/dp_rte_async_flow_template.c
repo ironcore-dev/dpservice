70824a1babcf5cf9be7015353bb7eccc5e97157c
Jaromír Smrček <4plague@gmail.com>
2024-07-03 16:56:13 +0200

    Multiport eswitch cleanup

--- src/rte_flow/dp_rte_async_flow_template.c
+++ src/rte_flow/dp_rte_async_flow_template.c
@@ -1,94 +1,134 @@
 // SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
-#include "dp_port.h"
-
-#include "rte_flow/dp_rte_async_flow.h"
 #include "rte_flow/dp_rte_async_flow_template.h"
+
+#include <rte_flow.h>
+
+#include "dp_port.h"
+#include "rte_flow/dp_rte_async_flow.h"
 #include "rte_flow/dp_rte_flow_helpers.h"
 
-static const struct rte_flow_pattern_template_attr default_pattern_template_attr = {
-	.ingress = 1
-};
 
-static const struct rte_flow_actions_template_attr default_action_template_attr = {
-	.ingress = 1
-};
+struct dp_port_async_template *dp_alloc_async_template(uint8_t pattern_count, uint8_t actions_count)
+{
+	struct dp_port_async_template *tmpl;
 
-static const struct rte_flow_template_table_attr pf_default_table_attr = {
-		.flow_attr = {
-			.group = 0,
-			.ingress = 1,
-		},
-		.nb_flows = DP_ASYNC_FLOW_PF_DEFAULT_TABLE_MAX_RULES,
-		// .specialize = RTE_FLOW_TABLE_SPECIALIZE_TRANSFER_WIRE_ORIG,
-};
+	tmpl = rte_zmalloc("async_template", sizeof(*tmpl), RTE_CACHE_LINE_SIZE);
+	if (!tmpl) {
+		DPS_LOG_ERR("Failed to allocate an async flow template");
+		return NULL;
+	}
 
-// static const struct rte_flow_template_table_attr pf_tunnel_table_attr = {
-// 		.flow_attr = {
-// 			.group = 2,
-// 			.transfer = 1,
-// 		},
-// 		.nb_flows = DP_ASYNC_FLOW_TABLE_MAX_RULES,
-// 		.specialize = RTE_FLOW_TABLE_SPECIALIZE_TRANSFER_WIRE_ORIG,
-// }; // this is not used due to the fact that async ipinip is not supported by mellanox yet
+	tmpl->pattern_templates = rte_zmalloc("async_template_pattern", sizeof(*tmpl->pattern_templates), RTE_CACHE_LINE_SIZE);
+	if (!tmpl->pattern_templates) {
+		DPS_LOG_ERR("Failed to allocate an async flow template pattern");
+		rte_free(tmpl);
+		return NULL;
+	}
 
-static int dp_create_pf_default_async_rule_templates(uint16_t port_id) {
+	tmpl->actions_templates = rte_zmalloc("async_template_actions", sizeof(*tmpl->actions_templates), RTE_CACHE_LINE_SIZE);
+	if (!tmpl->actions_templates) {
+		DPS_LOG_ERR("Failed to allocate an async flow template actions");
+		rte_free(tmpl->pattern_templates);
+		rte_free(tmpl);
+		return NULL;
+	}
 
-	struct rte_flow_item pattern[3];
-	int pattern_cnt = 0;
+	tmpl->pattern_count = pattern_count;
+	tmpl->actions_count = actions_count;
+	return tmpl;
+}
 
-	struct rte_flow_action action[2];
-	struct rte_flow_action action_mask[2];
-	int action_cnt = 0;
-	int action_mask_cnt = 0;
+int dp_init_async_template(uint16_t port_id, struct dp_port_async_template *tmpl)
+{
+	struct rte_flow_error error;
 
-	struct rte_flow_pattern_template_attr pattern_template_attr = default_pattern_template_attr;
-	struct rte_flow_actions_template_attr action_template_attr = default_action_template_attr;
-	int ret;
+	for (uint8_t i = 0; i < tmpl->pattern_count; ++i) {
+		if (!tmpl->pattern_templates[i]) {
+			DPS_LOG_ERR("Incomplete async template patterns");
+			return DP_ERROR;
+		}
+	}
+	for (uint8_t i = 0; i < tmpl->actions_count; ++i) {
+		if (!tmpl->actions_templates[i]) {
+			DPS_LOG_ERR("Incomplete async template actions");
+			return DP_ERROR;
+		}
+	}
 
-	// create match pattern template: IP in IPv6 tunnel packets by only matching the proto field in ipv6 header
-	dp_set_eth_flow_item(&pattern[pattern_cnt++], NULL, 0, DP_SET_FLOW_ITEM_WITH_MASK);
-	dp_set_ipv6_flow_item(&pattern[pattern_cnt++], NULL, 0, DP_SET_FLOW_ITEM_WITH_MASK);
-	dp_set_end_flow_item(&pattern[pattern_cnt++]);
-
-	ret = dp_rte_async_create_pattern_template(port_id, pattern, &pattern_template_attr,
-										DP_ASYNC_TEMPLATE_TABLE_PF_ISOLATION, DP_ASYNC_TEMPLATE_PATTERN_PF_IPV6_PROTO);
-	if (DP_FAILED(ret))
+	tmpl->template_table = rte_flow_template_table_create(port_id, tmpl->table_attr,
+															  tmpl->pattern_templates, tmpl->pattern_count,
+															  tmpl->actions_templates, tmpl->actions_count,
+															  &error);
+	if (!tmpl->template_table) {
+		DPS_LOG_ERR("Failed to create async flow template table",
+					DP_LOG_RET(rte_errno), DP_LOG_PORTID(port_id), DP_LOG_FLOW_ERROR(error.message));
 		return DP_ERROR;
-
-	// create flow action: allow packets to enter dp-service packet queue
-	dp_set_redirect_queue_action(&action[action_cnt++], NULL, 0);
-	dp_set_end_action(&action[action_cnt++]);
-
-	dp_set_redirect_queue_action(&action_mask[action_mask_cnt++], NULL, 0);
-	dp_set_end_action(&action_mask[action_mask_cnt++]);
-
-	ret = dp_rte_async_create_actions_template(port_id, action, action_mask, &action_template_attr,
-										DP_ASYNC_TEMPLATE_TABLE_PF_ISOLATION, DP_ASYNC_TEMPLATE_ACTION_PF_QUEUE);
-	if (DP_FAILED(ret))
-		return DP_ERROR;
-
-	dp_rte_async_create_table_attribute(port_id, DP_ASYNC_TEMPLATE_TABLE_PF_ISOLATION, &pf_default_table_attr);
+	}
 
 	return DP_OK;
 }
 
-int dp_create_pf_async_rte_rule_templates(uint16_t port_id) {
+void dp_destroy_async_template(uint16_t port_id, struct dp_port_async_template *tmpl)
+{
+	struct rte_flow_error error;
+	int ret;
 
-	DPS_LOG_INFO("Installing async rule templates", DP_LOG_PORTID(port_id));
+	if (!tmpl)
+		return;
 
-	if (DP_FAILED(dp_create_pf_default_async_rule_templates(port_id))) {
-		DPS_LOG_ERR("Failed to create pf async rte rule templates", DP_LOG_PORTID(port_id));
-		return DP_ERROR;
+	if (tmpl->template_table) {
+		ret = rte_flow_template_table_destroy(port_id, tmpl->template_table, &error);
+		if (DP_FAILED(ret))
+			DPS_LOG_ERR("Failed to destroy async template table", DP_LOG_RET(ret), DP_LOG_PORTID(port_id), DP_LOG_FLOW_ERROR(error.message));
 	}
-
-	// add more template pattern/action combinations in the future
-
-	if (DP_FAILED(dp_rte_async_create_template_tables(port_id))) {
-		DPS_LOG_ERR("Failed to create pf async rte rule template table", DP_LOG_PORTID(port_id));
-		return DP_ERROR;
+	for (uint8_t i = 0; i < tmpl->actions_count; ++i) {
+		if (tmpl->actions_templates[i]) {
+			ret = rte_flow_actions_template_destroy(port_id, tmpl->actions_templates[i], &error);
+			if (DP_FAILED(ret))
+				DPS_LOG_ERR("Failed to destroy async actions template", DP_LOG_RET(ret), DP_LOG_PORTID(port_id), DP_LOG_FLOW_ERROR(error.message));
+		}
 	}
+	for (uint8_t i = 0; i < tmpl->pattern_count; ++i) {
+		if (tmpl->pattern_templates[i]) {
+			ret = rte_flow_pattern_template_destroy(port_id, tmpl->pattern_templates[i], &error);
+			if (DP_FAILED(ret))
+				DPS_LOG_ERR("Failed to destroy async pattern template", DP_LOG_RET(ret), DP_LOG_PORTID(port_id), DP_LOG_FLOW_ERROR(error.message));
+		}
+	}
+	rte_free(tmpl->actions_templates);
+	rte_free(tmpl->pattern_templates);
+	rte_free(tmpl);
+}
 
-	return DP_OK;
+
+struct rte_flow_pattern_template *dp_create_async_pattern_template(uint16_t port_id,
+																   const struct rte_flow_pattern_template_attr *pattern_template_attr,									const struct rte_flow_item pattern[])
+{
+	struct rte_flow_pattern_template *pattern_template;
+	struct rte_flow_error error;
+
+	pattern_template = rte_flow_pattern_template_create(port_id, pattern_template_attr, pattern, &error);
+	if (!pattern_template)
+		DPS_LOG_ERR("Failed to create async flow pattern template",
+					DP_LOG_RET(rte_errno), DP_LOG_PORTID(port_id), DP_LOG_FLOW_ERROR(error.message));
+
+	return pattern_template;
+}
+
+struct rte_flow_actions_template *dp_create_async_actions_template(uint16_t port_id,
+																   const struct rte_flow_actions_template_attr *actions_template_attr,
+																   const struct rte_flow_action actions[],
+																   const struct rte_flow_action masks[])
+{
+	struct rte_flow_actions_template *actions_template;
+	struct rte_flow_error error;
+
+	actions_template = rte_flow_actions_template_create(port_id, actions_template_attr, actions, masks, &error);
+	if (!actions_template)
+		DPS_LOG_ERR("Failed to create async flow action template",
+					DP_LOG_RET(rte_errno), DP_LOG_PORTID(port_id), DP_LOG_FLOW_ERROR(error.message));
+
+	return actions_template;
 }
