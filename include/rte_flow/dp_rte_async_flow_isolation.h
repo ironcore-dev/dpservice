334d154b87e732162179100fc02f7ca8738f34cd
Jaromír Smrček <4plague@gmail.com>
2024-07-10 17:57:19 +0200

    Implement multiport eswitch for virtual services

--- include/rte_flow/dp_rte_async_flow_isolation.h
+++ include/rte_flow/dp_rte_async_flow_isolation.h
@@ -17,6 +17,14 @@ int dp_create_pf_async_isolation_templates(struct dp_port *port);
 
 int dp_create_pf_async_isolation_rules(struct dp_port *port);
 
+#ifdef ENABLE_VIRTSVC
+int dp_create_virtsvc_async_isolation_templates(struct dp_port *port, uint8_t proto_id);
+
+struct rte_flow *dp_create_virtsvc_async_isolation_rule(uint16_t port_id, uint8_t proto_id,
+														const union dp_ipv6 *svc_ipv6, rte_be16_t svc_port,
+														struct rte_flow_template_table *template_table);
+#endif
+
 #ifdef __cplusplus
 }
 #endif
