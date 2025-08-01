dee842a6182860994818aad2f34e42bd1030cf98
Jaromír Smrček <4plague@gmail.com>
2025-08-01 18:05:46 +0200

    Sync node modes of operation

---
+++ include/nodes/sync_node.h
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef __INCLUDE_SYNC_NODE_H__
+#define __INCLUDE_SYNC_NODE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void sync_node_switch_role(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
