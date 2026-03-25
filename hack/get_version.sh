535dfac82893a25d5dd516e08989207f9233f7db
florin-alexandru.peter@t-systems.com <florin-alexandru.peter@t-systems.com>
2026-03-25 09:22:21 +0100

    Add DPSERVICE_VERSION build arg to override version during Docker build

--- hack/get_version.sh
+++ hack/get_version.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Allow overriding the version via environment variable
+if [ -n "$DPSERVICE_VERSION" ]; then
+    echo "$DPSERVICE_VERSION"
+    exit 0
+fi
+
 # For non-tagged commits, this is a simple "vX.X.X-XX-gXXXXXXX"
 # For dpservice-bin tagged commits, this is "vX.X.X"
 # For dpservice-go tagged commits, this is "go/dpservice-go/vX.X.X-XX-gXXXXXXX"
