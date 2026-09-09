cdc6051aeb177c7f43521b71cd342e11336a1555
Felix Riegger <felix.riegger@sap.com>
2026-09-09 10:40:13 +0200

    Remove year from license headers
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- hack/set_cap.sh
+++ hack/set_cap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+# SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 # SPDX-License-Identifier: Apache-2.0
 
 # NET_ADMIN for setting MAC, etc.
--- src/rte_flow/dp_rte_flow_capture.c
+++ src/rte_flow/dp_rte_flow_capture.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
 #include "rte_flow/dp_rte_flow_capture.h"
